b315ced39145c99a2a22424f19fc1ad6ea5607bc
Helio Loureiro <helio.loureiro@xensam.com>
2025-08-18 17:25:10 +0200

    First version
     - not working and a lot of hardcoded stuff

---
+++ Makefile
@@ -0,0 +1,61 @@
+PROJECT := nginx-openmetrics
+RELEASE := 1.0
+DEBVERSION := 1
+ARCH := amd64
+
+BIN_LINUX := $(PROJECT)
+SRC := $(PROJECT)/main.go
+
+CONTACT := helio@loureiro.eng.br
+
+
+BUILD_OPTIONS = -modcacherw
+BUILD_OPTIONS += -ldflags="-w -X 'main.Version=$$(git tag -l --sort taggerdate | tail -1)'"
+BUILD_OPTIONS += -buildmode=pie
+BUILD_OPTIONS += -tags netgo,osusergo
+BUILD_OPTIONS += -trimpath
+
+REGISTRY := registry.xensam.eu/images/$(PROJECT)
+
+all: $(BIN_LINUX)
+
+init:
+	go mod tidy
+	go mod vendor
+
+test: init $(SRC)
+	cd $(PROJECT)
+	go test -v ./...
+
+$(BIN_LINUX): init $(SRC) test
+	cd $(PROJECT)
+	env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o $(BIN_LINUX) $(BUILD_OPTIONS) ./...
+
+container:
+	docker build -t $(PROJECT) .
+	img_id=$(shell docker images --format=json | jq -r '. | select(.Repository == "$(PROJECT)") | .ID ') ;\
+	git_tag=$(shell ./vas.sh print_tag) ;\
+	docker tag $$img_id $(REGISTRY):$$git_tag
+	#docker push $()
+
+
+tag:
+	NEWVERSION=$(shell expr $(DEBVERSION) + 1 ); \
+		sed -i "/^DEBVERSION/ s/:= .*/:= $$NEWVERSION/" Makefile
+	make debian
+	make git_tag
+	# generate proper version
+	make clean debian
+
+git_tag:
+	git commit -a -m "Uplifiting to version: $(RELEASE)-$(DEBVERSION)"
+	last_commit=$(shell git log --pretty=format:"%H" | head -1); \
+		git tag "$(RELEASE)-$(DEBVERSION)" $$last_commit
+
+push:
+	git push origin HEAD
+	git push origin HEAD --tags
+
+clean:
+	rm -f $(PROJECT)/$(BIN_LINUX)
+	rm -f *deb *dsc *build *buildinfo *changes *xz
---
+++ nginx-openmetrics/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	Version = "development"
+
+	activeConnections = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "active_connections",
+		Help: "The number of active connections",
+	})
+
+	serverAccepts = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "server_accepts_total",
+		Help: "The total number of server accepted connections",
+	})
+
+	serverHandled = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "server_handled_total",
+		Help: "The total number of server handled connections",
+	})
+
+	serverRequests = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "server_requests_total",
+		Help: "The total number of server requests",
+	})
+
+	connectionsReading = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "reading_connections",
+		Help: "The number of active reading connections",
+	})
+
+	connectionsWriting = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "writing_connections",
+		Help: "The number of active writing connections",
+	})
+
+	connectionsWaiting = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "waiting_connections",
+		Help: "The number of waiting connections",
+	})
+)
+
+func main() {
+	service := flag.String("service", "http://localhost:8080/api", "the nginx api service, like http://localhost:8080/api")
+	port := flag.Int("port", 9090, "default port to listen the service")
+	printVersion := flag.Bool("version", false, "print the version and exit")
+	flag.Parse()
+
+	if *printVersion {
+		fmt.Println("nginx-open-metrics-service")
+		fmt.Println("version:", Version)
+		os.Exit(0)
+	}
+
+	if *service == "" {
+		log.Fatal("missing service service")
+	}
+
+	fmt.Println("fetching data from:", *service)
+	fmt.Println("starting service at port:", *port)
+	/*
+	 * Active connections: 39
+	 * server accepts handled requests
+	 * 286479 286479 1417563
+	 * Reading: 0 Writing: 64 Waiting: 10
+	 */
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
+}
